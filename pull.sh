4caee5e7b5fccfb3ddbbe43fae38082ce5f4828c
marcus <marcus@email.com>
2025-10-31 17:34:21 +0100

    pull from appvm before pulling to dom0

--- pull.sh
+++ pull.sh
@@ -8,5 +8,6 @@ AppVM=dotfiles
 # init the repo on dom0
 # git clone -b ${BRANCH} /tmp/git.bundle
 
+qvm-run -u user --pass-io ${AppVM} "cd ${REPO} && git pull"
 qvm-run -u user --pass-io ${AppVM} "cd ${REPO} && git bundle create - ${BRANCH}" > /tmp/git.bundle
 git pull -r
