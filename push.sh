450a4623bfd834c9bfa95f38a33e44586cbdc31a
Marcus Birgersson <marcus.s.birgersson@gmail.com>
2025-10-31 16:07:08 +0100

    bugfix in pull, make push and pull executable

--- push.sh
+++ push.sh
@@ -7,7 +7,7 @@ AppVM=dotfiles
 # setup on the AppVM
 # git remote add dom0 /tmp/dom0.bundle
 
-git bundle create - origin/${BRANCH}..${BRANCH} | \
+git bundle create - ${BRANCH} | \
   qvm-run -u user --pass-io ${AppVM} "cat > /tmp/dom0.bundle"
 
 qvm-run -u user --pass-io ${AppVM} "cd ${REPO} && git pull -r /tmp/dom0.bundle ${BRANCH}"
